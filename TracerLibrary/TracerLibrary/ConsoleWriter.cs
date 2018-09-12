0a8442824b307f601b5f7e0540a33d2e68f7a699
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-09-12 16:07:37 +0300

    added consoleWriter and serializer

---
+++ TracerLibrary/TracerLibrary/ConsoleWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace TracerLibrary
+{
+    public interface IConsoleWriter
+    {
+        void consoleWrite(object o);
+    }
+
+    public class ConsoleWriter : IConsoleWriter
+    {
+        public XmlSerializer newXmlFormatter = new XmlSerializer();
+        public DataContractJsonSerializer newJSONFormatter = new DataContractJsonSerializer();
+
+        public void consoleWrite(object o)
+        {
+            newXmlFormatter.Serialize(Console.Out, o);
+            newJSONFormatter.WriteObject(Console.Out, o);
+        }
+    }
+}
---
+++ TracerLibrary/TracerLibrary/Serializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace TracerLibrary
+{
+    public interface ISerializer
+    {
+        void serialize(object o);
+    }
+
+    public class Serializer : ISerializer
+    {
+        private const string xmlFilePath = "xmlResilt.xml";
+        private const string jsonFilePath = "jsonResilt.json";
+        public XmlSerializer newXmlFormatter = new XmlSerializer();
+        public DataContractJsonSerializer newJSONFormatter = new DataContractJsonSerializer();
+
+        public void serialize(object someObject)
+        {
+            using (FileStream fs = new FileStream(xmlFilePath, FileMode.OpenOrCreate))
+            {
+                newXmlFormatter.Serialize(fs, someObject);
+            }
+
+            using (FileStream fs = new FileStream(jsonFilePath, FileMode.OpenOrCreate))
+            {
+                newJSONFormatter.WriteObject(fs, someObject);
+            }
+        }
+    }
+}
